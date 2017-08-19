fb438ea415daf9f9ad9bcd620a1d1c127e7ade7a
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-08-19 18:10:29 -0500

    Yelp file made and you're able to search through node

---
+++ bin/yelp.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const yelp = require('yelp-fusion');
+
+const token = yelp.accessToken('HE07TPuDAVs_O6pJsI9z6g', 'vX6oRVnFpWz2rF22RBCmxGEIzdEVmE8kEXdKn9QMAWoPgQz1DNgaAWlAUqMFIXoh').then(response => {
+  // console.log(response.jsonBody.access_token);
+}).catch(e => {
+  // console.log(e);
+});
+
+const client = yelp.client('oJ5UEBNgziEmy5_wUULQqDi1AIUu9Ew1EZ17InQEV-1uO_ZN3gw7jESfs7LO6cABvf5IXluKmd_TzgZUuHwhLMZ7SP1IDtpjVsM82UZOun0CD_6sremos4E0NL2YWXYx');
+
+client.search({
+  term:'coffee',
+  location: 'san francisco, ca'
+}).then(response => {
+  console.log(response.jsonBody.businesses[0].name);
+}).catch(e => {
+  console.log(e);
+});
+
+client.autocomplete({
+  text:'pizza'
+}).then(response => {
+  console.log(response.jsonBody.terms[0].text);
+}).catch(e => {
+  console.log(e);
+});
