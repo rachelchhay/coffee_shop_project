3ad38fbdc85a762e35499e0cfb998fe1bb04c5ff
Richard Neely <richardneely@Richards-Mac-Pro.local>
2017-08-24 10:48:33 -0400

    extra progress on socket.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require ('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+console.log('index.js');
 
 app.use(express.static('public'));
 
 
-// app.get('/', function(req, res){
-// res.sendFile(__dirname +'/index.html');
-// });
+app.get('/', function(req, res){
+res.sendFile(__dirname +'/index.html');
+});
 
 
 
